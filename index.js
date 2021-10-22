64b9c43ede125a4018678ba2eede1385890c4a09
Douglas Valente dos Santos <douglasvalente6@gmail.com>
2021-10-21 22:00:09 -0300

    adiciona API do GitHub para a troca de informações do crachá

---
+++ index.js
@@ -0,0 +1,17 @@
+const userGitHub = window.prompt(`Digite seu usuário do GitHub`)
+
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/${userGitHub}`
+
+    fetch(url)
+    .then(response =>  response.json())
+    .then(data => {
+        userName.textContent = data.name,
+        userPhoto.src = data.avatar_url,
+        container_my_github.href = data.html_url,
+        userLink.textContent = data.login,
+        userBio.textContent = data.bio
+    })
+}
+
+getGitHubProfileInfos()
